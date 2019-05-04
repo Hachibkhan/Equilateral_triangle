60f8ea87467d9ff05304373f105642552be1ed39
Hachibkhan <hachib.khan19@gmail.com>
2019-05-04 18:23:30 +0600

    first commit

---
+++ equilateral_triangle.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.speed(2)
+
+turtle.color("green")
+
+turtle.forward(130)
+
+turtle.left(120)
+
+turtle.forward(130)
+
+turtle.left(120)
+
+turtle.forward(130)
+
+turtle.left(120)
+
+turtle.exitonclick()
+
+
+
+
